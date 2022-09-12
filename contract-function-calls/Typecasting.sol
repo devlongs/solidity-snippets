993a5f4774be0609fd1325d4888c9ee42258898f
Longs Pemun Gotar <developerlongs@gmail.com>
2022-09-12 17:20:25 +0000

    Calling a contract function through typecasting

---
+++ contract-function-calls/Typecasting.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.8.9;
+
+contract A {
+    uint256 public a;
+
+    function increament(uint256 _a) public {
+        a = _a;
+    }
+
+    function setA(uint256 _a) public payable {
+        require(msg.value != 0);
+        a = _a;
+    }
+}
+
+interface AInterface {
+    function increament(uint256 _a) external;
+
+    function setA(uint256 _a) external payable;
+}
+
+contract B {
+    function callIncreament(address _contract) public {
+        AInterface aContract = AInterface(_contract);
+
+        aContract.increament(12);
+    }
+
+    function callSetA(address _contract) public payable {
+        A aContract = A(_contract);
+        aContract.setA.value(msg.value)(12);
+    }
+}
